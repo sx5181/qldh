f466afe7ef737bc445e4d785a71d4f3fec678e3c
sx5181 <thaisonmdc@gmail.com>
2017-02-21 08:59:36 +0700

    qldh-first
    qldh-initial

---
+++ MvcApplication1/MvcApplication1/Controllers/TinBaiController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MvcApplication1.Models;
+using Newtonsoft.Json;
+
+namespace MvcApplication1.Controllers
+{
+    public class TinBaiController : ApiController
+    {
+        databaseDataContext db = new databaseDataContext(ConfigurationManager.ConnectionStrings["QLDHConnectionString"].ConnectionString);
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            List<TinBaiViet> lst = db.TinBaiViets.ToList();
+            string json = JsonConvert.SerializeObject(lst);
+            return new string[] { json };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
